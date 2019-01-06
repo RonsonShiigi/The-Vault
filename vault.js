d7fa7e6e0ea0a49858542e18311703db846e463c
Ronson Shiigi <ronsonshiigi@Ronsons-MBP.home>
2019-01-05 15:38:53 -1000

    pau

--- vault.js
+++ vault.js
@@ -1,4 +1,27 @@
 'use strict';
 module.exports = function() {
-  
+    let obj ={};
+    
+       
+        function getValue (key){
+            if(key in obj){
+                
+                return obj[key]
+                
+            }else{
+                
+                return null}
+            
+        };
+        function setValue(key,value) {
+            obj[key]=value;
+            return obj.key
+        }
+            
+        return {
+            getValue,
+            setValue,
+        }
+
+    
 };
